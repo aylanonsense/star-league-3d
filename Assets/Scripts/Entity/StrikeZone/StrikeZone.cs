0fc7780415e3b096b34328f6145af5d50262934a
bridgs <bridgs.dev@gmail.com>
2020-01-09 12:27:04 -0500

    Strike zone freaking out in a good way

---
+++ Assets/Scripts/Entity/StrikeZone/StrikeZone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class StrikeZone : Entity
+    {
+        [SerializeField] private GameObject gridSprite;
+        [SerializeField] private GameObject targetSprite;
+
+        private SpriteController gridSpriteController;
+        private SpriteController targetSpriteController;
+
+        public override void Created()
+        {
+            gridSpriteController = gridSprite.GetComponent<SpriteController>();
+            targetSpriteController = targetSprite.GetComponent<SpriteController>();
+        }
+
+        public void SetTarget(int x, int y)
+        {
+            targetSpriteController.SetPixelPosition(26.0f * x, 26.0f * y);
+        }
+
+        public override void PrepareToRender()
+        {
+            SetTarget(SensibleMath.Range(-1, 1), SensibleMath.Range(-1, 1));
+        }
+    }
+}
---
+++ Assets/Scripts/Render/SpriteController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteController : BaseMonoBehaviour
+    {
+        [SerializeField] private SpriteSheet spriteSheet;
+
+        private SpriteRenderer spriteRenderer;
+        private Sprite[] sprites;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            if (spriteSheet != null) {
+                sprites = spriteSheet.GetSprites();
+            }
+        }
+
+        public int GetNumSprites()
+        {
+            return sprites.Length;
+        }
+
+        public void SetSprite(int index = 0)
+        {
+            spriteRenderer.sprite = sprites[index];
+        }
+
+        public Vector3 GetPixelPosition()
+        {
+            Vector3 pos = transform.localPosition;
+            Vector3 scale = transform.localScale;
+            return new Vector3(pos.x * 32.0f / scale.x, pos.y * 32.0f / scale.y, pos.z * 32.0f / scale.z);
+        }
+
+        public void SetPixelPosition(float x = 0.00f, float y = 0.00f, float z = 0.00f)
+        {
+            Vector3 scale = transform.localScale;
+            transform.localPosition = new Vector3(x * scale.x / 32.0f, y * scale.y /32.0f, z * scale.z / 32.0f);
+        }
+    }
+}
